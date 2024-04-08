def4c7946eba1ab2dff4c4d78406a93ee6edd6dd
snehaKgn <sneha.parmar@kgn-software.com>
2024-04-08 17:27:19 +0530

    Category Update category insert/update done

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -33,46 +33,128 @@ class CategoriesController extends Controller
                 echo '<br>';
                 echo "update";
                 echo '<br>';
+                $magento_id = array_search($category_url, $MagentoCategories);
+                $this->updateCategoryInMagento($magento_id,$catCsvArr[$i]);
             }else{
                 echo '<br>';
                 echo "INSERT";
                 echo '<br>';
-                $this->insertCategoryInMagento($catCsvArr[$i]);
+                $result = $this->insertCategoryInMagento($catCsvArr[$i]);
+                if(!empty($result)){
+                    $MagentoCategories[$result['magento_id']] = $result['magento_url'];
+                }
             }
         }
         echo 'All category updated.';
         die();
     }
 
-    function findMagentoParentId($parentContentId){
-        $data = DB::table('category')->where('data_category_id', $parentContentId)->get()->first();
-        if(!empty((array)$data)){
-            return $data->magento_category_id;
-        }else{
-            return '2';
-        }
-        
-    }
-
     function insertCategoryInMagento($csvCatData){
         $resultLogId = '';
-        $status = '';
+        $resultLogId = $this->generateLog($csvCatData['ContentID'],'catagory');
+        $data = $this->getCategoryData($csvCatData);
+
         $result = array();
-        $defaultCategory = 2;
         $this->service = configMagento();
         $this->service->init();
+        try {
+            $result = $this->service->call('categories', $data, 'POST');
+        } catch (\Throwable $e) {
+            echo 'Failed to insert category to magento for content ID '.$csvCatData['ContentID'];
+            Log::info("Failed to insert category to magento");
+            Log::info($e);
+        }    
 
+        $CurrentTime = date("Y-m-d H:i:s");
+        $dataCatTblArr = array();
+        $logIdArr=array();
+        $returnArr=array();
+        $logIdArr['logId'] = $resultLogId;
+        if(isset($result->id)){
+            $dataCatTblArr['magento_id'] = $result->id;
+            $dataCatTblArr['content_id'] = $csvCatData['ContentID'];
+            $dataCatTblArr['magento_parent_id'] = $result->parent_id;
+            $dataCatTblArr['status'] = 'success';
+            $dataCatTblArr['updated_at'] = $CurrentTime;
+            $dataCatTblArr['created_at'] = $CurrentTime;
+            
+            $logIdArr['status'] = 'success';
+            $logIdArr['message'] = 'Data has been inserted.';
+
+            $returnArr['magento_id'] = $result->id;
+            $returnArr['magento_url'] = $category_url;
+        }else{
+            $dataCatTblArr['magento_id'] = 0;
+            $dataCatTblArr['content_id'] = $csvCatData['ContentID'];
+            $dataCatTblArr['magento_parent_id'] = 0;
+            $dataCatTblArr['status'] = 'failed';
+            $dataCatTblArr['updated_at'] = $CurrentTime;
+            $dataCatTblArr['created_at'] = $CurrentTime;
+            
+            $logIdArr['status'] = 'failed';
+            $logIdArr['message'] = 'Process failed';
+        }
+        $this->insertCatTbl($dataCatTblArr);
+        $this->updateGeneratedLog($logIdArr);
+        
+        return $returnArr;
+    }
+
+    function updateCategoryInMagento($magento_id,$csvCatData){
+        $resultLogId = '';
+        $resultLogId = $this->generateLog($csvCatData['ContentID'],'catagory');
+        $data = $this->getCategoryData($csvCatData);
+
+        $result = array();
+        $this->service = configMagentoUpdate();
+        $this->service->init();
+        try {
+            $restUrl = 'categories/'.$magento_id;
+            $result = $this->service->call($restUrl, $data, 'PUT');
+        } catch (\Throwable $e) {
+            $proceesMsg = 'Failed to insert category to magento for content ID '.$csvCatData['ContentID'];
+            Log::info("Failed to insert category to magento");
+            Log::info($e);
+            echo $proceesMsg;
+        }    
+
+        $CurrentTime = date("Y-m-d H:i:s");
+        $dataCatTblArr = array();
+        $logIdArr=array();
+        $logIdArr['logId'] = $resultLogId;
+        if(isset($result->id)){
+            $dataCatTblArr['magento_id'] = $result->id;
+            $dataCatTblArr['content_id'] = $csvCatData['ContentID'];
+            $dataCatTblArr['magento_parent_id'] = $result->parent_id;
+            $dataCatTblArr['status'] = 'success';
+            $dataCatTblArr['updated_at'] = $CurrentTime;
+            $dataCatTblArr['created_at'] = $CurrentTime;
+            
+            $logIdArr['status'] = 'success';
+            $logIdArr['message'] = 'Data has been inserted.';
+        }else{
+            $dataCatTblArr['magento_id'] = 0;
+            $dataCatTblArr['content_id'] = $csvCatData['ContentID'];
+            $dataCatTblArr['magento_parent_id'] = 0;
+            $dataCatTblArr['status'] = 'failed';
+            $dataCatTblArr['updated_at'] = $CurrentTime;
+            $dataCatTblArr['created_at'] = $CurrentTime;
+            
+            $logIdArr['status'] = 'failed';
+            $logIdArr['message'] = $proceesMsg;
+        }
+        $this->updateCatTbl($magento_id,$dataCatTblArr);
+        $this->updateGeneratedLog($logIdArr);
+    }
+
+    public function getCategoryData($csvCatData){
 
         $parentContentId = $csvCatData['ParentContentID'];
         $magentoParentId = $this->findMagentoParentId($parentContentId);
 
-        $resultLogId = $this->generateLog($csvCatData['ContentID'],'catagory');
-        /*echo '<pre>'; print_r($csvCatData); echo '</pre>'; die();*/
-
         $category_url = $csvCatData['FileName'];
         $urlArr = array_filter(explode('/', $category_url));
         $currentCatUrl = end($urlArr);
-
 
         $is_active='false'; if($csvCatData['Enabled']==1){ $is_active='true';}
         $custom_attributes[] = (object)array(
@@ -101,44 +183,7 @@ class CategoriesController extends Controller
         );
         $magentoData = (object)array('category' => $categoryData);
         $data = $magentoData;
-
-        /*echo '<pre>'; print_r($data); echo '</pre>'; die();*/
-
-        try {
-            $result = $this->service->call('categories', $data, 'POST');
-        } catch (\Throwable $e) {
-            echo 'Failed to insert category to magento for content ID '.$csvCatData['ContentID'];
-            Log::info("Failed to insert category to magento");
-            Log::info($e);
-        }    
-
-        $CurrentTime = date("Y-m-d H:i:s");
-        $dataCatTblArr = array();
-        $logIdArr=array();
-        $logIdArr['logId'] = $resultLogId;
-        if(isset($result->id)){
-            $dataCatTblArr['magento_id'] = $result->id;
-            $dataCatTblArr['content_id'] = $csvCatData['ContentID'];
-            $dataCatTblArr['magento_parent_id'] = $result->parent_id;
-            $dataCatTblArr['status'] = 'success';
-            $dataCatTblArr['updated_at'] = $CurrentTime;
-            $dataCatTblArr['created_at'] = $CurrentTime;
-            
-            $logIdArr['status'] = 'success';
-            $logIdArr['message'] = 'Data has been inserted.';
-        }else{
-            $dataCatTblArr['magento_id'] = 0;
-            $dataCatTblArr['content_id'] = $csvCatData['ContentID'];
-            $dataCatTblArr['magento_parent_id'] = 0;
-            $dataCatTblArr['status'] = 'failed';
-            $dataCatTblArr['updated_at'] = $CurrentTime;
-            $dataCatTblArr['created_at'] = $CurrentTime;
-            
-            $logIdArr['status'] = 'failed';
-            $logIdArr['message'] = 'Process failed';
-        }
-        $this->insertCatTbl($dataCatTblArr);
-        $this->updateGeneratedLog($logIdArr);
+        return $data;
     }
 
     public function generateLog($contentid, $type){
@@ -154,6 +199,7 @@ class CategoriesController extends Controller
         $resultLogId = DB::table('log')->insertGetId($values);
         return $resultLogId;
     }
+
     public function updateGeneratedLog($logIdArr){
         DB::table('log')->where('id', $logIdArr['logId'])->update(['status' => $logIdArr['status'],'message' => $logIdArr['message'],'flag' => 0]);
         return;
@@ -168,6 +214,12 @@ class CategoriesController extends Controller
                         'created_at' => $dataCatTblArr['created_at']
                     );
         DB::table('category')->insert($values);
+        return;
+    }
+
+    public function updateCatTbl($magentoId,$dataCatTblArr){
+
+        DB::table('category')->where('magento_category_id', $magentoId)->update(['data_category_id' => $dataCatTblArr['content_id'], 'magento_parent_id' => $dataCatTblArr['magento_parent_id'],'status' => $dataCatTblArr['status'],'updated_at' => $dataCatTblArr['updated_at']]);
         return;
     }
 
@@ -200,45 +252,14 @@ class CategoriesController extends Controller
         }
     }
 
-    public function checkCategoryIsAvailableInMagento123($category_url){
-        try {
-            $this->service = configMagento();
-            $this->service->init();
-            $result = $this->service->call("categories/list?searchCriteria");
-
-            if(isset($result->total_count) && $result->total_count > 0) {
-                $removeValuesAr = array("root catalog", "default category");
-                $removeValuesIdsAr = array();
-                $pathAr = array();
-                foreach ($result->items as $iKey => $itemAr) {
-                    if(!in_array(strtolower($itemAr->name), $removeValuesAr)) {
-                        $pathAr[$itemAr->id] = $itemAr->name;
-                        $pathNameAr = array();
-                        foreach (explode('/', $itemAr->path) as $pValue) {
-                            if(!in_array($pValue, $removeValuesIdsAr) && isset($pathAr[$pValue])) {
-                                $pathNameAr[] = $pathAr[$pValue];
-                            }
-                        }
-                        $dataAr = array(
-                            'magento_id' => $itemAr->id,
-                            'parent_id' => $itemAr->parent_id,
-                            'name' => $itemAr->name,
-                            'path' => implode('/', array_diff(explode('/', $itemAr->path), $removeValuesIdsAr)),
-                            'path_name' => implode(" : ", $pathNameAr)
-                        );
-                        $this->magentoCategory->insertUpdateCategory($dataAr);
-                    } else {
-                        $removeValuesIdsAr[] = $itemAr->id;
-                    }
-                }
-            }
-            return view('dashboard');
-        } catch (\Throwable $e) {
-            Log::info("Failed to retrieve categories from magento");
-            Log::info($e);
-            return view('dashboard');
-            return false;
+    public function findMagentoParentId($parentContentId){
+        $data = DB::table('category')->where('data_category_id', $parentContentId)->get()->first();
+        if(!empty((array)$data)){
+            return $data->magento_category_id;
+        }else{
+            return '2';
         }
+        
     }
     
 }
--- app/helpers.php
+++ app/helpers.php
@@ -19,3 +19,20 @@ function configMagento()
 
     return $service;
 }
+function configMagentoUpdate()
+{
+    $service = new \Experius\Magento2ApiClient\Service\RestApi();
+
+    // Oauth Signing
+    $service->setConsumerKey('w9f0vidhbl7dboy2f2qrvc6243bm618y');
+    $service->setConsumerSecret('efwo6iaxfsm0vwn9ukt0ug1y8zty2wz8');
+    $service->setAccesToken('m998dqcngdsshiau1mdztzdygyp3lp5c');
+    $service->setAccesTokenSecret('3z2s6ulyajdy9oir5fo3phksvwy85pea');
+
+    $service->setUrl('https://magento244.dev-box.me/rest/%storecode/V1/');
+
+    // OPTIONAL > default = all
+    $service->setStoreCode('default');
+
+    return $service;
+}
